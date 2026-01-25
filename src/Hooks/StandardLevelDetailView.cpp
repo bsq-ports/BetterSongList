be84e9314edd945bafe6f07ce7396eb3e03a39be
Alex Uskov <FrozenAlex@users.noreply.github.com>
2026-01-25 06:38:33 +0400

    Revert orig hook, brings back the bug with duplicating beatleader ui, will fix it in beatleader

--- src/Hooks/StandardLevelDetailView.cpp
+++ src/Hooks/StandardLevelDetailView.cpp
@@ -6,7 +6,7 @@
 
 // from UI/SongDeleteButton
 // from UI/ExtraLevelParams
-MAKE_AUTO_HOOK_ORIG_MATCH(StandardLevelDetailView_RefreshContent, &GlobalNamespace::StandardLevelDetailView::RefreshContent, void, GlobalNamespace::StandardLevelDetailView* self)
+MAKE_AUTO_HOOK_MATCH(StandardLevelDetailView_RefreshContent, &GlobalNamespace::StandardLevelDetailView::RefreshContent, void, GlobalNamespace::StandardLevelDetailView* self)
 {
     StandardLevelDetailView_RefreshContent(self);
     BetterSongList::Hooks::ExtraLevelParams::StandardLevelDetailView_RefreshContent_Postfix(self, self->_beatmapLevel, self->beatmapKey, self->_levelParamsPanel);
